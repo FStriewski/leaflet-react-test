660bfd76ae94011c11a9c9d56b20961622bc2c03
FStriewski <fstriewski@outlook.com>
2018-04-29 13:46:18 +0200

    Basic project setup

---
+++ src/Map.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Map extends Component {
+
+    render (){
+        return (
+            <div>
+                <h4> This is the map </h4>
+            </div>
+
+        )
+
+    }
+
+
+
+
+}
